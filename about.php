67574c4a973609773958e7976eafe98941c2b103
Éric Gillet <eric.gillet@linuxw.info>
2014-02-08 00:51:12 +0100

    Add carousel, various style fixes, add footer, use static CSS + JS

--- about.php
+++ about.php
@@ -1,57 +1,97 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>RobotWithMe - Inscription </title>
+    <title>RobotWithMe - About us </title>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
 
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="./css/sticky_footer.css">
+
+    <link rel="stylesheet" type="text/css" href="./css/jcarousel.basic.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.block label { display: inline-block; width: 100px; text-align: right;}
-</style>
+
+    <style type="text/css">
+        .block label { 
+            display: inline-block;
+            width: 100px;
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="./">RobotWithMe</a>
+    <div id="wrap">
+        <div class="navbar navbar-inverse navbar-fixed-top wrap" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="./about.php">RobotWithMe</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="./about.php">About us</a></li>
+                        <li><a href="./products.php">Products</a></li>
+                        <li><a href="./register.php">Register</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
             </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="./about.php">A Propos de nous</a></li>
-                    <li><a href="./products.php">Produits</a></li>
-                    <li><a href="./register.php">Inscription</a></li>
+        </div>
+        <div class="container">
+
+            <div class="jcarousel-wrapper">
+                <div class="jcarousel">
+                    <ul>
+                        <li><img src="./img/gabe.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="./img/jackson.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="./img/gabe.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="./img/jackson.jpg" width="600" height="400" alt=""></li>
+                    </ul>
+                </div>
+
+                <p class="photo-credits">
+                    Photos by <a href="http://www.perdu.com">Internet</a>
+                </p>
+
+                    <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+                    <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+                     <p class="jcarousel-pagination">
+                        
+                    </p>
+            </div>
+
+            <div class="well">
+                <h1>Who we are</h1>
+                <p>We are a ...</p>
+            </div>
+            <div class="well">
+                <h1>Sponsors</h1>
+                <ul>
+                    <li>Valve</li><li>CocaCola</li><li>Doritos</li><li>Monster</li>
                 </ul>
-            </div><!--/.nav-collapse -->
+            </div>
+            <div class="well">
+                <h1>Contact us</h1>
+                <p>eric@linux.com <br> 6 allée de GabeN</p>
+            </div>
         </div>
+        <div id="push"></div>
     </div>
-    <div class="container">
-        <div class="well">
-            <h1><center>Carousel</center></h1>
-        </div>
-        <div class="well">
-            <h1>Who we are</h1>
-            <p>We are a ...</p>
-        </div>
-        <div class="well">
-            <h1>Sponsors</h1>
-            <ul>
-                <li>Valve</li><li>CocaCola</li><li>Doritos</li><li>Monster</li>
-            </ul>
-        </div>
-        <div class="well">
-            <h1>Contact</h1>
-            <p>eric@linux.com <br> 6 allée de GabeN</p>
-        </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="credit">Copyright 2014 Mathieu Degaine & <a href="http://me.linuxw.info">Éric Gillet</a>.</p>
+      </div>
     </div>
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="./js/jquery.jcarousel.min.js"></script>
+    <script type="text/javascript" src="./js/jcarousel.basic.js"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -27,57 +27,72 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>RobotWithMe - Inscription </title>
+	<title>RobotWithMe - Register </title>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
 
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.block label { 
-	display: inline-block; width: 100px; text-align: right;
-}
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="./css/sticky_footer.css">
 
-.green {
-	color:green;
-	font-weight:bold;
-}
-</style>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		.block {
+			margin-bottom: 8px;
+		}
+
+		.block label { 
+			display: inline-block; width: 100px; text-align: right;
+		}
+
+		.green {
+			color:green;
+			font-weight:bold;
+		}
+	</style>
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div id="wrap">
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="./about.php">RobotWithMe</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="./about.php">About us</a></li>
+						<li><a href="./products.php">Products</a></li>
+						<li class="active"><a href="./register.php">Register</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="./">RobotWithMe</a>
-			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="./about.php">A Propos de nous</a></li>
-					<li><a href="./products.php">Produits</a></li>
-					<li class="active"><a href="./register.php">Inscription</a></li>
-				</ul>
+			<div class="well">
+				<?php if($valid_data) echo "<span class=\"green\">Successfully registered</span>";?>
+				<form action="./register.php" method="post">
+					<div class="block"><label>First Name:</label> 	<input type="text" name="name" required autofocus></div>
+					<div class="block"><label>Last Name:</label>	<input type="text" name="surname" required></div>
+					<div class="block"><label>Age:</label>			<input type="number" name="age" min="13" max="120" required></div>
+					<div class="block"><label>Email:</label>		<input type="email" name="mail" required></div>
+					<input type="submit" value="S'enregistrer">
+				</form>
 			</div>
 		</div>
+		<div id="push"></div>
+    </div>
+
+	<div id="footer">
+	      <div class="container">
+	        <p class="credit">Copyright 2014 Mathieu Degaine & <a href="http://me.linuxw.info">Éric Gillet</a>.<a id="credits" href="./credits.php">Credits</a></p>
 	</div>
-	<div class="container">
-		<div class="well">
-			<?php if($valid_data) echo "<span class=\"green\">Successfully registered</span>";?>
-			<form action="./register.php" method="post">
-				<div class="block"><label>First Name:</label> 	<input type="text" name="name" required autofocus></div>
-				<div class="block"><label>Last Name:</label>	<input type="text" name="surname" required></div>
-				<div class="block"><label>Age:</label>			<input type="number" name="age" min="13" max="120" required></div>
-				<div class="block"><label>Email:</label>		<input type="email" name="mail" required></div>
-				<input type="submit" value="S'enregistrer">
-			</form>
-		</div>
-	</div>
-	<script src="https://code.jquery.com/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+
+	<script src="./js/jquery.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
 </body>
 </html>
