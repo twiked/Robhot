441e622af932fe148941732b9b934d296da11504
Éric Gillet <eric.gillet@linuxw.info>
2014-02-08 00:10:12 +0100

    Add credits page, and fill correctly LICENSE file

---
+++ credits.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>RobotWithMe - About us </title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
+
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
+
+    <link rel="stylesheet" type="text/css" href="./css/jcarousel.basic.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.block label { display: inline-block; width: 100px; text-align: right;}
+</style>
+</head>
+<body>
+	<h1>Sticky footer :</h1>
+	<p>Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+
+	<h1>Bootstrap</h1>
+
+	<h1>jCarousel</h1>
+	<p><a href="http://sorgalla.com/jcarousel/"></a></p>
+</body>
