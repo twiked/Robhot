de7853be14f21a83b4dfa2b5b6ed411755ca819c
Éric Gillet <eric.gillet@linuxw.info>
2014-02-08 16:11:49 +0100

    Add products page stub

---
+++ products.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>RobotWithMe - Product </title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
+
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="./css/sticky_footer.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css">
+        .product {
+            margin-top: 30px;
+        }
+
+        .center {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div id="wrap">
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="./about.php">RobotWithMe</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="./about.php">About us</a></li>
+                        <li class="active"><a href="./products.php">Products</a></li>
+                        <li><a href="./register.php">Register</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="well product row">
+                <div class="col-md-4">
+                    <img class="center" src="http://2.bp.blogspot.com/-2T-jEJdK6RM/T3KZBdisH3I/AAAAAAAAGMc/CGa6EtWunB0/s1600/1%2B(2).jpg" width=200 height=200>
+                </div>
+                <div class="col-md-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+                <div class="col-md-4 center">
+                    <h1>9001 BTC</h1>
+                    <br>
+                    <a href="#"> Buy</a>
+                </div>
+            </div>
+        </div>
+        <div id="push"></div>
+    </div>
+
+    <div id="footer">
+        <div class="container">
+            <p class="credit">Copyright 2014 Mathieu Degaine & <a href="http://me.linuxw.info">Éric Gillet</a>.<a id="credits" href="./credits.php">Credits</a></p>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="./js/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+</body>
+</html>
