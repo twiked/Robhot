8ec7b768f1c6c8de4fcbf4abe42510a359387010
Yolompy <matdeg69@gmail.com>
2014-02-21 13:48:34 +0100

    adding article number

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <title>Aperture Science</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="css/style_form.css">
+</head>
+<body>
+<header>Aperture Science</header>
+
+    <div id="menu">
+        <li>A Propos de nous</li>
+        <li>Produits</li>
+        <li>Inscription</li>
+    </div>
+</body>
