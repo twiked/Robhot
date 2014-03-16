4e272fd772024ecb341b7c938ca6f44f445a37ad
Éric Gillet <eric.gillet@linuxw.info>
2014-03-16 21:06:22 +0100

    Add more db variables to ease configuration

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
 
+$db_host = 'localhost';
+$db_name = 'web';
 $db_login = 'web';
 $db_password = 'web';
 
 try {
-	$db = new PDO('mysql:host=localhost;dbname=web', $db_login, $db_password);
+	$db = new PDO(sprintf('mysql:host=%s;dbname=%s', $db_host, $db_name), $db_login, $db_password);
 } catch (Exception $e) {
 	echo 'ERROR: ' . $e->getMessage();
 }
